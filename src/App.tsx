b6cd92b7e608a1c8a6d687e5dcdb199a6a1bd451
Frederik Wulf <frederikw@compusoftgroup.com>
2022-04-02 12:05:54 +0200

    added sections

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,17 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <section>
+        <h1>Section 1</h1>
+      </section>
+      <section style={{backgroundColor: 'black'}}>
+        <h1>Section 2</h1>
+      </section>
+      <section>
+        <h1>Section 3</h1>
+      </section>
     </div>
   );
 }
