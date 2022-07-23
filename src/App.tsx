2cc91b322816faf2e0825bfe5865997c53ab8ce9
Frederik Wulf <frederikw@compusoftgroup.com>
2022-07-23 13:47:04 +0200

    added snap scroll

--- src/App.tsx
+++ src/App.tsx
@@ -10,8 +10,7 @@ function App() {
   const refSection3 = useRef<HTMLElement>(null);
   const refSection4 = useRef<HTMLElement>(null);
   const [activePageIndex, setActivePageIndex] = useState(0);
-  let lastScrollTop = 0;
-  let isScrolling = false;
+
   const scrollTo = (index: number) => {
     setActivePageIndex(index);
     switch (index) {
@@ -32,54 +31,68 @@ function App() {
   }
 
   useEffect(() => {
-    window.addEventListener('wheel', (ev) => {
-      console.log(true);
-      ev.preventDefault();
-      let scrollTop = document.documentElement.scrollTop;
-      let newIndex = activePageIndex;
-      if (scrollTop > lastScrollTop){
-        // scroll down
-        if (activePageIndex < 4) {
-          newIndex = activePageIndex + 1;
+    let isScrolling: any;
+
+// Listen for scroll events
+    window.addEventListener('scroll', ( event ) => {
+      // Clear our timeout throughout the scroll
+      window.clearTimeout(isScrolling);
+
+      // Set a timeout to run after scrolling ends
+      isScrolling = setTimeout(() => {
+
+        if (refSection1.current) {
+          if (isElementVisible(refSection1?.current)) setActivePageIndex(0);
         }
-      } else {
-        // scroll up
-        if (activePageIndex > 0) {
-          newIndex = activePageIndex - 1;
+        if (refSection2.current) {
+          if (isElementVisible(refSection2?.current)) setActivePageIndex(1);
         }
-      }
-      scrollTo(activePageIndex);
-      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
-    })
-  })
+        if (refSection3.current) {
+          if (isElementVisible(refSection3?.current)) setActivePageIndex(2);
+        }
+        if (refSection4.current) {
+          if (isElementVisible(refSection4?.current)) setActivePageIndex(3);
+        }
+
+      }, 66);
+
+    }, false);
+  });
 
     return (
       <div>
         <TopNavigation activeIndex={activePageIndex} onClick={scrollTo}/>
         <Navigation activeIndex={activePageIndex} onSectionClick={scrollTo}/>
-        <section id={"section1"} ref={refSection1}>
-          <div className={'section-content'}>
-            <h1>About</h1>
-          </div>
-        </section>
-        <section id={"section2"} ref={refSection2}>
-          <div className={'section-content'} style={{backgroundColor: 'grey'}}>
-            <h1>Portfolio</h1>
-          </div>
-        </section>
-        <section id={"section3"} ref={refSection3}>
-          <div className={'section-content'}>
-            <h1>Resume</h1>
-          </div>
-        </section>
-        <section id={"section4"} ref={refSection4}>
-          <div className={'section-content'}>
-            <Footer />
-          </div>
-        </section>
+        <div className={'wrapper'}>
+          <section id={"section1"} ref={refSection1}>
+            <div className={'section-content'}>
+              <h1>About</h1>
+            </div>
+          </section>
+          <section id={"section2"} ref={refSection2}>
+            <div className={'section-content'} style={{backgroundColor: 'grey'}}>
+              <h1>Portfolio</h1>
+            </div>
+          </section>
+          <section id={"section3"} ref={refSection3}>
+            <div className={'section-content'}>
+              <h1>Resume</h1>
+            </div>
+          </section>
+          <section id={"section4"} ref={refSection4}>
+            <div className={'section-content'}>
+              <Footer />
+            </div>
+          </section>
+        </div>
 
       </div>
     );
+}
+
+const isElementVisible = (el: HTMLElement) => {
+  let rect     = el.getBoundingClientRect();
+  return  rect.y === 0;
 }
 
 export default App;
